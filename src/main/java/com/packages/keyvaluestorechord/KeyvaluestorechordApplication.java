21388ed6d4a3bdc784048bbfb1d5c638e7ea888d
“PriyankaGD” <priyanka.dastidar@gmail.com>
2024-10-29 10:46:00 -0700

    Initial commit

---
+++ src/main/java/com/packages/keyvaluestorechord/KeyvaluestorechordApplication.java
@@ -0,0 +1,13 @@
+package com.packages.keyvaluestorechord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyvaluestorechordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyvaluestorechordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/packages/keyvaluestorechord/KeyvaluestorechordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.packages.keyvaluestorechord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyvaluestorechordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
